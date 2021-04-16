856e0a3685ad00f7326c489ab446b7b5e6e6269e
DanielsZeps <77250340+DanielsZeps@users.noreply.github.com>
2021-04-16 14:56:42 +0300

    Add files via upload

---
+++ script/render_article.js
@@ -0,0 +1,86 @@
+function articleTitle(value) {
+  path = "fileStorage/" + value["path"];
+  window.scrollTo(0, 0)
+  try {
+    var article = document.getElementById("article");
+    article.innerHTML += "";
+  } catch {
+    article = document.createElement("div");
+    article.id = "article";
+    document.body.appendChild(article);
+    var div_1 = document.createElement("div");
+    article.appendChild(div_1);
+    var div_2 = document.createElement("div");
+    article.appendChild(div_2);
+  }
+  if (value["image"]) {
+    
+    document.body.style.backgroundImage = "url(\"" + path + "/image_1.jpeg" + "\")";
+
+    if (value["type"] == "type1") {
+      backgroundImageSrc = new Image()
+      backgroundImageSrc.onload = function() {
+        if (this.width > window.innerWidth && window.innerWidth < 1000) {
+          document.body.style.backgroundSize = "contain, auto";
+        } else {
+          document.body.style.backgroundSize = null;
+        }
+        article.style.marginTop = (window.innerWidth / this.width * (this.height - 40)) + "px";
+      }
+      backgroundImageSrc.src = path + "/image_1.jpeg";
+      backgroundImageSrc = path + "/image_1.jpeg";
+    } else {
+      backgroundImageSrc = "type2";
+    }
+  } else {
+    document.body.style.backgroundImage = null;
+
+    if (value["type"] == "type1") {
+      backgroundImageSrc = new Image()
+      backgroundImageSrc.onload = function() {
+        if (this.width > window.innerWidth && window.innerWidth < 1000) {
+          document.body.style.backgroundSize = "contain, auto";
+        } else {
+          document.body.style.backgroundSize = null;
+        }
+        article.style.marginTop = (window.innerWidth / this.width * (this.height - 40)) + "px";
+      }
+      backgroundImageSrc.src = "fileStorage/stirniene/image_1.jpeg";
+      backgroundImageSrc = "fileStorage/stirniene/image_1.jpeg";
+    } else {
+      backgroundImageSrc = "type2";
+    }
+  }
+  if (value["type"] == "type1") {
+    article.className = "articleType1";
+    document.body.style.backgroundPosition = null;
+    document.body.style.backgroundSize = null;
+    document.body.style.backgroundRepeat = null;
+    document.body.style.backgroundAttachment = null;
+  } else if (value["type"] == "type2") {
+    article.className = "articleType2";
+    article.style.marginTop = document.getElementById("navigation").getBoundingClientRect().height;
+    const img = new Image();
+    img.onload = function() {
+      document.body.style.backgroundPosition = "right top";
+      document.body.style.backgroundSize = "50% auto";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundAttachment = "fixed";
+    }
+    img.src = path + "/image_1.jpeg";
+  }
+  document.getElementById("article").type = value["type"];
+
+  var h1 = document.createElement("h1");
+  h1.appendChild( document.createTextNode(value["title"]) );
+  article.getElementsByTagName("div")[0].innerHTML = null;
+  article.getElementsByTagName("div")[0].appendChild(h1);
+
+  var x = value["text"].split("\n");
+  article.getElementsByTagName("div")[1].innerHTML = null;
+  for (var y = 0; y < x.length; y++) {
+    var p = document.createElement("p");
+    p.appendChild( document.createTextNode(x[y]) );
+    article.getElementsByTagName("div")[1].appendChild(p);
+  }
+}
